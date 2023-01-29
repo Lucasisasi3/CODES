331dd2a426904aa555d57af07601f07d7453f1bf
Lucasisasi3 <122904556+Lucasisasi3@users.noreply.github.com>
2023-01-29 22:13:56 +0800

    Update index.php

--- CHAT BOT UNIVERSITY/index.php
+++ CHAT BOT UNIVERSITY/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ChatBot with PHP</title>
-    <link rel="stylesheet" href="http://localhost/bot2/css/estilo.css">
+    <link rel="stylesheet" href="http://localhost/CHAT BOT UNIVERSITY/css/estilo.css">
 </head>
 
 <body>
